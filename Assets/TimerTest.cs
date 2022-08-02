5efaf8be5c98adc5f658365fd2dc2e4475899b1a
oguzhankurtoglu <66673273+oguzhankurtoglu@users.noreply.github.com>
2022-08-02 19:31:34 +0300

    dependencies are gone

--- Assets/TimerTest.cs
+++ Assets/TimerTest.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 
@@ -8,22 +6,6 @@ public class TimerTest : MonoBehaviour
 {
     private void Start()
     {
-        Delay.RunAfter(2f, () =>
-        {
-            Debug.Log("started");
-        } );
-    }
-
-    private void Movement()
-    {
-        Delay.RunAfter(1f,()=> Debug.Log("moved"));
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Movement();
-        }
+       this.RunAfter(2f,()=>Debug.Log("test"));
     }
 }
